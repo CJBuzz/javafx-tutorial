98b31dafe26ddd3bff46ac3ecf4f149d845b17f0
CJ <cj1000buzz@gmail.com>
2026-08-19 09:42:04 +0800

    Add image avatar and dialog box (JavaFX Tutorial part 2)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,28 +1,87 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
+import java.net.URL;
+
 
 public class Main extends Application {
 
-    private static final String DEFAULT_FILE_PATH = "duke/example.txt";
+    private ScrollPane scrollPane;
+    private VBox dialogContainer;
+    private TextField userInput;
+    private Button sendButton;
+    private Scene scene;
+
+    private final Image userImage = loadImage("/images/DaUser.png");
+    private final Image dukeImage = loadImage("/images/DaDuke.png");
+
+    private static Image loadImage(String resourcePath) {
+        URL resource = Main.class.getResource(resourcePath);
+        if (resource == null) {
+            throw new IllegalStateException("Image resource not found: " + resourcePath);
+        }
+        return new Image(resource.toExternalForm());
+    }
 
     @Override
     public void start(Stage stage) {
-        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
-        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
+        //Setting up required components
 
-        stage.setScene(scene); // Setting the stage to show our scene
-        stage.show(); // Render the stage.
-    }
+        scrollPane = new ScrollPane();
+        dialogContainer = new VBox();
+        scrollPane.setContent(dialogContainer);
 
-    // Existing constructor
-    public Main(String filePath) {
-        // ...
-    }
+        userInput = new TextField();
+        sendButton = new Button("Send");
 
-    // Overloaded constructor
-    public Main() {
-        this(DEFAULT_FILE_PATH);
+        DialogBox dialogBox = new DialogBox("Hello!", userImage);
+        dialogContainer.getChildren().addAll(dialogBox);
+
+        AnchorPane mainLayout = new AnchorPane();
+        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+
+        scene = new Scene(mainLayout);
+
+        // Style
+        stage.setTitle("Duke");
+        stage.setResizable(false);
+        stage.setMinHeight(600.0);
+        stage.setMinWidth(400.0);
+
+        mainLayout.setPrefSize(400.0, 600.0);
+
+        scrollPane.setPrefSize(385, 535);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+
+        scrollPane.setVvalue(1.0);
+        scrollPane.setFitToWidth(true);
+
+        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
+
+        userInput.setPrefWidth(325.0);
+
+        sendButton.setPrefWidth(55.0);
+
+        AnchorPane.setTopAnchor(scrollPane, 1.0);
+
+        AnchorPane.setBottomAnchor(sendButton, 1.0);
+        AnchorPane.setRightAnchor(sendButton, 1.0);
+
+        AnchorPane.setLeftAnchor(userInput, 1.0);
+        AnchorPane.setBottomAnchor(userInput, 1.0);
+
+        stage.setScene(scene);
+        stage.show();
+
+        //More code to be added here later
     }
 }
